52638bc26faddc18e580f6ff7b5f182e3546b0dc
darknawg63 <darknawg63@gmail.com>
2015-04-12 13:36:14 +0200

    Played around with 3x4 column divs

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+<?php
+$posts = ['What on earth do you think might be wrong with the computer sytems in this day in age after all this time?', 'Where is Erik?'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <!-- Basic Page Needs
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <meta charset="utf-8">
-  <title>Skeleton :)</title>
+  <title>Skeleton Haha</title>
   <meta name="Pilot project on Skeleton" content="">
   <meta name="darknawg63" content="">
 
@@ -20,6 +23,7 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="stylesheet" href="public/css/normalize.css">
   <link rel="stylesheet" href="public/css/skeleton.css">
+  <link rel="stylesheet" href="public/css/layout.css">
 
   <!-- Favicon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -28,11 +32,24 @@
 <body>
   <!-- .container is main centered wrapper -->
   <div class="container">
-    <div class="row">
-      <div class="one column">First</div>
-      <div class="four columns">Second</div>
-      <div class="four columns">Third</div>
+    <div id="nav" class="twelve columns">
+    <header>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
     </div>
-  </div>
+    </header>
+    <h1>Blogger <small><?php echo 'My own shameless plug...'; ?></small></h1>
+    <hr>
+    <?php foreach ($posts as $post): ?>
+    <div class="row">
+      <div class="eight columns"><span><?php echo $post; ?></span><p><i>02-12-2014</i></p></div>
+      <div class="four columns"><span></span></div>
+    </div>
+    <hr>
+  <?php endforeach ?>
 </body>
 </html>
